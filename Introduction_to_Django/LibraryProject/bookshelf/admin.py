8c39114368d899c9dc6cdbb0de78eb78a4961dee
Gelagay G. <gela.sirta3@gmail.com>
2025-09-01 00:13:19 +0300

    CRUD Operations on django project

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,4 +3,17 @@ from .models import Book
 
 # Register your models here.
 admin.site.register(Book)
-admin.ModelAdmin
+
+
+
+# @admin.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    # Display these fields in the list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # Add filters to the sidebar
+    list_filter = ('author', 'publication_year')
+
+    # Add a search bar
+    search_fields = ('title', 'author')
