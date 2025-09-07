4d22d5cdbc9c4c73af9346368439c18b03b1a731
Gelagay G. <gela.sirta3@gmail.com>
2025-09-07 18:40:37 +0300

    Add relationship_app with models, views, and templates for library management

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Author, Book, Library, Librarian
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name')
+    search_fields = ('name',)
+
+
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+# Function-based view: List all books
+def list_books(request):
+    books = Book.objects.select_related("author").all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+
+# Class-based view: Show library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
